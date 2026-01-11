f7c3fe8502320e480cfb348c405acf2ae89d2511
abjohnson117 <abjohnson117@gmail.com>
2026-01-10 18:25:16 -0800

    Got results for ode and sde

--- banana/ksd.py
+++ banana/ksd.py
@@ -129,14 +129,15 @@ def stein_kernel_jax(X, score_func, bandwidth, kernel="rbf"):
         k = get_gaussianRBF(bandwidth)
     kvec = vectorize_kfunc(k)
     K = kvec(X, X)
-    pairwise_dists = (-1 * jnp.log(K)) * (2 * bandwidth ** 2)
+    # pairwise_dists = (-1 * jnp.log(K)) * (2 * bandwidth ** 2)
     # print(f"These are the first 5 elements of pairwise_dists: {pairwise_dists[0, :5]}") # same
 
     diff = X[:, None, :] - X[None, :, :]  # shape (n, m, d)
+    sqdist = jnp.sum(diff ** 2, axis=-1)
     K_grad = -diff / (bandwidth ** 2) * K[:, :, None]
     # print(f"These are the first 5 elements of K_grad: {K_grad[0, :5]}") #same
     # compute second trace derivative (Hessian) of the kernel
-    trK_grad2 = K * (pairwise_dists - X.shape[1] * bandwidth**2) / (bandwidth**4)  # (n, m)
+    trK_grad2 = K * (sqdist - X.shape[1] * bandwidth**2) / (bandwidth**4)  # (n, m)
     # print(f"this is trace of hessian: {trK_grad2}") #same
     
     score_X = score_func(X)
@@ -156,10 +157,17 @@ def compute_ksd(X, score_func, bandwidth=None, kernel='rbf'):
     ksd = np.sum(H) / (X.shape[0] ** 2)
     return ksd
 
+# def compute_ksd_jax(X, score_func, bandwidth=None, kernel="rbf"):
+#     """Computes the Kernel Stein Discrepancy for samples X (target) using jax functionality"""
+#     H = stein_kernel_jax(X, score_func, bandwidth, kernel=kernel)
+#     ksd2 = np.sum(H) / (X.shape[0] ** 2)
+#     return ksd2
 def compute_ksd_jax(X, score_func, bandwidth=None, kernel="rbf"):
     """Computes the Kernel Stein Discrepancy for samples X (target) using jax functionality"""
     H = stein_kernel_jax(X, score_func, bandwidth, kernel=kernel)
-    ksd2 = np.sum(H) / (X.shape[0] ** 2)
+    H = H - jnp.diag(jnp.diag(H))
+    ksd2 = jnp.sum(H) / (X.shape[0] * (X.shape[0] - 1))
+    # ksd2 = np.sum(H) / (X.shape[0] ** 2)
     return ksd2
 
 if __name__ == "__main__":
